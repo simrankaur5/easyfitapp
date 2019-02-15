79e7e9956c0f058793e0a67dd1c070e2f399823c
Michal Michal <kawon@hotmail.co.uk>
2019-02-15 20:07:08 +0000

    Initial Commit

---
+++ EasyFitApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EasyFitApp
+//
+//  Created by Michal Nowak on 15/02/2019.
+//  Copyright © 2019 Michal Nowak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

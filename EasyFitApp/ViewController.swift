bb69c563eb4e2418ebf6ab6b706d5f544c55ebc3
Michal Michal <kawon@hotmail.co.uk>
2019-02-16 13:43:56 +0000

    Login working registering not so much yet

--- EasyFitApp/ViewController.swift
+++ EasyFitApp/ViewController.swift
@@ -27,23 +27,38 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
     @IBAction func registerPressed(_ sender: Any) {
-//        if let registerEmail = registerEmailTextField.text , let registerPass = passwordTextField.text{
-      if  let email = registerEmailTextField.text, let pass = registerPasswordTextField.text
+      if  let email = registerEmailTextField.text, let pass = registerPasswordTextField.text, let confirmPass = registerConfirmPasswordTextField.text
       {
+        if pass == confirmPass{
+        
+        
                 Auth.auth().createUser(withEmail: email, password: pass) { (user, error) in
                     
                     if user != nil{
                         self.performSegue(withIdentifier: "goHome", sender: self)
                         
                     }
+                    else{
+                        self.createAlert(title: "Error", message: "Error")
+                    }
                 }
-            
         }
-           
+        else{
+            createAlert(title: "Error", message: "Your passwords do no match")
+        }
+        }
+      
         
     }
     
-   
+    func createAlert(title:String , message:String ){
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+        alert.addAction(UIAlertAction(title: "ok", style: UIAlertAction.Style.default, handler: { (action) in
+            alert.dismiss(animated:true, completion:nil)
+            
+        }))
+        self.present(alert, animated: true, completion: nil)
+    }
     
     @IBAction func LoginTapped(_ sender: UIButton) {
         if let email = emailTextField.text, let pass = passwordTextField.text
@@ -56,43 +71,12 @@ class ViewController: UIViewController {
                 }
                 else {
                     //error check error and show message
+                    self.createAlert(title: "Error", message: "Error")
                 }
             }
         }
         
         }
-        //Get the default auth ui obj
-//        let authUI = FUIAuth.defaultAuthUI()
-//        guard authUI != nil else{
-//            return
-//        }
-        
-        //Set ourselves as delegate
-//        authUI?.delegate = self
-//        //Get a refference  to the auth ui view controller
-//        let authViewController = authUI!.authViewController()
-//        //show
-//        present(authViewController, animated: true, completion: nil)
-//    }
+
+
 }
-//override func touchesBegan(_ touches: Set<UITouch>, with event:UIEvent?){
-//    //Dismiss keyboard when view is tapped
-//    emailTextField.resignFirstResponder()
-//    passwordTextField.resignFirstResponder()
-//
-//}
-
-
-//extension ViewController: FUIAuthDelegate{
-//    func authUI(_ authUI: FUIAuth, didSignInWith authDataResult: AuthDataResult?, error: Error?) {
-//        //Check for error
-//        guard error == nil else{
-//            //log error
-//            return
-//        }
-//        //authDataResult?.user.uid
-//        performSegue(withIdentifier: "goHome", sender: self)
-//    }
-//
-//}
-

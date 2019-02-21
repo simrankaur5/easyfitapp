acf6413c79174229fd505974d9dc4cc30811ae03
Tom Mason <tmaso001@gold.ac.uk>
2019-02-21 19:54:03 +0000

    Reading from database for welcome message

--- EasyFitApp/HomeViewController.swift
+++ EasyFitApp/HomeViewController.swift
@@ -24,11 +24,41 @@ class HomeViewController: UIViewController {
         
         // Do any additional setup after loading the view.
         
-        var welcomeName:String = ""
         
-        let database_ref = Database.database().reference()
+        
+        var database_ref : DatabaseReference!
+        var database_handle : DatabaseHandle!
+        
+        database_ref = Database.database().reference()
 
-        database_ref.child("1001/name").setValue("TEST")
+        
+        
+         database_ref.child("1001").child("name").observeSingleEvent(of: .value, with: { (snapshot) in
+            // Get user value
+            let value : String = (snapshot.value as? String)!
+            self.welcomeLabel.text = "Welcome " + value
+          
+            
+            
+            // ...
+        }) { (error) in
+            print(error.localizedDescription)
+        }
+        
+        
+        
+//        database_handle = database_ref.child("1001/name").observe(.childAdded, with: { (data) in
+//            let name : String = (data.value as? String)!
+//            debugPrint(name)
+//        })
+        
+        //        welcomeLabel.text = w_Name
+//        welcomeLabel.text = String()
+        
+        
+        
+//        database_ref.child("1001/name").setValue("TEST")
+        
         //        database_ref.child("100001/Name").observeSingleEvent(of: .value, with: { (snapshot) in
 //            // Get welcome name
 //            let wName = snapshot.value as! String
@@ -38,8 +68,7 @@ class HomeViewController: UIViewController {
 //
 //        })
    
-//        welcomeLabel.text = w_Name
-        welcomeLabel.text = "Welcome " + welcomeName
+
         
         lvlProgress.tintColor = UIColor(red: 0, green: 0.8, blue: 0, alpha: 1.0)
         lvlProgress.trackTintColor = UIColor(red: 0.1, green: 0.1, blue: 0.1, alpha: 1.0)

96b2de970e4c1048edf1b91f37da2941fbcf8d60
Tom Mason <tmaso001@gold.ac.uk>
2019-02-21 00:28:05 +0000

    Read/Write to database

--- EasyFitApp/HomeViewController.swift
+++ EasyFitApp/HomeViewController.swift
@@ -1,30 +1,65 @@
-//
-//  HomeViewController.swift
-//  EasyFitApp
-//
-//  Created by Michal Nowak on 19/02/2019.
-//  Copyright © 2019 Michal Nowak. All rights reserved.
-//
 
 import UIKit
+import FirebaseDatabase
+
 
 class HomeViewController: UIViewController {
 
+    
+    @IBOutlet weak var welcomeLabel: UILabel!
+    
+    
+    
+    
+    @IBOutlet weak var lvlProgress: UIProgressView!
+    
+    @IBOutlet weak var calProgress: UIProgressView!
+    
+    //@IBOutlet weak var circleView: UIView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
+        
+        var welcomeName:String = ""
+        
+        let database_ref = Database.database().reference()
+
+        database_ref.child("1001/name").setValue("TEST")
+        //        database_ref.child("100001/Name").observeSingleEvent(of: .value, with: { (snapshot) in
+//            // Get welcome name
+//            let wName = snapshot.value as! String
+//
+//            welcomeName = wName
+//
+//
+//        })
+   
+//        welcomeLabel.text = w_Name
+        welcomeLabel.text = "Welcome " + welcomeName
+        
+        lvlProgress.tintColor = UIColor(red: 0, green: 0.8, blue: 0, alpha: 1.0)
+        lvlProgress.trackTintColor = UIColor(red: 0.1, green: 0.1, blue: 0.1, alpha: 1.0)
+        
+        //Scaling lvl progress bar
+        lvlProgress.transform =  lvlProgress.transform.scaledBy(x: 1,y: 30)
+        
+        
+        
+        
+        calProgress.tintColor = .yellow
+        calProgress.trackTintColor = UIColor(red: 0.1, green: 0.1, blue: 0.1, alpha: 1.0)
+        
+        
+        //Scaling cal progress bar
+        calProgress.transform =  calProgress.transform.scaledBy(x: 1,y: 30)
+        
+        
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
--- EasyFitApp/ViewController.swift
+++ EasyFitApp/ViewController.swift
@@ -191,55 +191,6 @@ class ViewController: UIViewController, FBSDKLoginButtonDelegate {
         
 
 
-}
-
-class homeViewController: UIViewController
-{
-    
-    @IBOutlet weak var welcomeLabel: UILabel!
-    
-  
-
-   
-    @IBOutlet weak var lvlProgress: UIProgressView!
-    
-    @IBOutlet weak var calProgress: UIProgressView!
-    
-    //@IBOutlet weak var circleView: UIView!
-    
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        // Do any additional setup after loading the view.
-        
-        //Reference name from database and change welcome Message
-        let welcomeName:String = "Tom"
-        welcomeLabel.text = "Welcome " + welcomeName
-        
-        
-        lvlProgress.tintColor = UIColor(red: 0, green: 0.8, blue: 0, alpha: 1.0)
-        lvlProgress.trackTintColor = UIColor(red: 0.1, green: 0.1, blue: 0.1, alpha: 1.0)
-        
-        //Scaling lvl progress bar
-        lvlProgress.transform =  lvlProgress.transform.scaledBy(x: 1,y: 30)
-        
-        lvlProgress.progress
-        
-        
-        calProgress.tintColor = .yellow
-        calProgress.trackTintColor = UIColor(red: 0.1, green: 0.1, blue: 0.1, alpha: 1.0)
-        
-        
-        //Scaling cal progress bar
-        calProgress.transform =  calProgress.transform.scaledBy(x: 1,y: 30)
-        
-     
-        
-    }
-    
-   
     
 
 }

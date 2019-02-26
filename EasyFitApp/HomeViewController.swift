ec7786f9fb4b6bf2043dfd120eedcc3e15e6722a
Tom Mason <tmaso001@gold.ac.uk>
2019-02-26 19:23:50 +0000

    Added new pod / changed home page ui / added new progress bar / made page dynamic

--- EasyFitApp/HomeViewController.swift
+++ EasyFitApp/HomeViewController.swift
@@ -5,6 +5,9 @@ import Firebase
 import FirebaseUI
 import FBSDKLoginKit
 import FBSDKCoreKit
+
+import MBCircularProgressBar
+
  var database_ref : DatabaseReference!
 class HomeViewController: UIViewController {
 
@@ -20,7 +23,7 @@ class HomeViewController: UIViewController {
     
     
     
-    @IBOutlet weak var lvlProgress: UIProgressView!
+
     
     @IBOutlet weak var calProgress: UIProgressView!
     
@@ -40,8 +43,16 @@ class HomeViewController: UIViewController {
 
         lvlLabel.font = lvlLabel.font.withSize(40)
         
-        lvlProgress.progress = 0
-        calProgress.progress = 0
+
+        calProgress.tintColor = .yellow
+        calProgress.trackTintColor = UIColor(red: 0.1, green: 0.1, blue: 0.1, alpha: 1.0)
+        
+        
+//        //Scaling cal progress bar
+//        calProgress.transform =  calProgress.transform.scaledBy(x: 1,y: 30)
+//
+        
+        calProgress.progress = 0.24
 //
 //        var database_ref : DatabaseReference!
 //
@@ -62,53 +73,53 @@ class HomeViewController: UIViewController {
 //            print(error.localizedDescription)
 //        }
         
-        
-        
-        database_ref.child(currentUser).observeSingleEvent(of: .value, with: { (snapshot) in
-  
-            //Snapshot NSDictionary
-            let value = snapshot.value as? NSDictionary
-            
-            //LVL VALUE
-            let lvlS = value?["lvl"] as! Int
-            
-            //XP VALUE
-            let xpS = value?["xp"] as! Int
-            
-            //CALORIES AND CALORIE LIMIT VALUE
-            let calorieS = value?["calories"] as! Float
-            let calorieLmtS = value?["calorie_limit"] as! Float
-            
-            //FLOAT VALUE FOR XP AND CALORIE PROGRESS VIEW
-            let xpProgress : Float = Float(xpS % 50)*0.02
-            let calProgress : Float = Float(calorieS) / Float(calorieLmtS)
-            
-            
-            //DISPLAY LEVEL
-            self.lvlLabel.text = String(lvlS)
-            
-            self.xpProgressLabel.text = String(Int(xpProgress*100)) + "%"
-            
-            self.calProgressLabel.text = String(Int(calorieS)) + " / " + String(Int(calorieLmtS))
-            
-            
-            //ANIMATE THE PROGRESSVIEW
-            UIView.animate(withDuration: 2.5) {
-                self.lvlProgress.setProgress(xpProgress, animated: true)
-                
-                self.calProgress.setProgress(calProgress, animated: true)
-            }
-            
-
-            
-            
-            
-            
-            // ...
-        }) { (error) in
-            print(error.localizedDescription)
-        }
-        
+//
+//
+//        database_ref.child(currentUser).observeSingleEvent(of: .value, with: { (snapshot) in
+//
+//            //Snapshot NSDictionary
+//            let value = snapshot.value as? NSDictionary
+//
+//            //LVL VALUE
+//            let lvlS = value?["lvl"] as! Int
+//
+//            //XP VALUE
+//            let xpS = value?["xp"] as! Int
+//
+//            //CALORIES AND CALORIE LIMIT VALUE
+//            let calorieS = value?["calories"] as! Float
+//            let calorieLmtS = value?["calorie_limit"] as! Float
+//
+//            //FLOAT VALUE FOR XP AND CALORIE PROGRESS VIEW
+//            let xpProgress : Float = Float(xpS % 50)*0.02
+//            let calProgress : Float = Float(calorieS) / Float(calorieLmtS)
+//
+//
+//            //DISPLAY LEVEL
+//            self.lvlLabel.text = String(lvlS)
+//
+//            self.xpProgressLabel.text = String(Int(xpProgress*100)) + "%"
+//
+//            self.calProgressLabel.text = String(Int(calorieS)) + " / " + String(Int(calorieLmtS))
+//
+//
+//            //ANIMATE THE PROGRESSVIEW
+//            UIView.animate(withDuration: 2.5) {
+//                self.lvlProgress.setProgress(xpProgress, animated: true)
+//
+//                self.calProgress.setProgress(calProgress, animated: true)
+//            }
+//
+//
+//
+//
+//
+//
+//            // ...
+//        }) { (error) in
+//            print(error.localizedDescription)
+//        }
+//
         var welcomeName = ""
         if (Auth.auth().currentUser?.displayName) != nil{
             welcomeName = (Auth.auth().currentUser?.email)!
@@ -117,23 +128,10 @@ class HomeViewController: UIViewController {
         welcomeLabel.adjustsFontSizeToFitWidth = true
         welcomeLabel.text = "Welcome " + welcomeName
         
-        lvlProgress.tintColor = UIColor(red: 0, green: 0.8, blue: 0, alpha: 1.0)
-        lvlProgress.trackTintColor = UIColor(red: 0.1, green: 0.1, blue: 0.1, alpha: 1.0)
-        
-        //Scaling lvl progress bar
-        lvlProgress.transform =  lvlProgress.transform.scaledBy(x: 1,y: 30)
+
         
         
-        
-        
-        calProgress.tintColor = .yellow
-        calProgress.trackTintColor = UIColor(red: 0.1, green: 0.1, blue: 0.1, alpha: 1.0)
-        
-        
-        //Scaling cal progress bar
-        calProgress.transform =  calProgress.transform.scaledBy(x: 1,y: 30)
-        
-        
+
         
     }
     

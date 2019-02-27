b6ec054f97eaee622d5bd7ece955c772a8d5a896
Michal Michal <kawon@hotmail.co.uk>
2019-02-27 18:25:56 +0000

    changes

--- EasyFitApp/AppDelegate.swift
+++ EasyFitApp/AppDelegate.swift
@@ -18,9 +18,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
         FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
         // Override point for customization after application launch.
         FirebaseApp.configure()
+
+    
         return true
     }
     func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {

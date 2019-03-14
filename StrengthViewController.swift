bca0548a3b14e2639d51b84533fcb4b6363d7b43
Amin Ahmadi <aahma004@gold.ac.uk>
2019-03-14 14:43:26 +0000

    strength view controller

---
+++ StrengthViewController.swift
@@ -0,0 +1,39 @@
+//
+//  StrengthViewController.swift
+//  EasyFitApp
+//
+//  Created by Amin Ahmadi on 04/03/2019.
+//  Copyright © 2019 Michal Nowak. All rights reserved.
+//
+
+import UIKit
+
+class StrengthViewController: UIViewController {
+    @IBAction func currentWorkoutButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func addNewWorkout(_ sender: UIButton) {
+    }
+    @IBAction func history(_ sender: UIButton) {
+    }
+    @IBAction func preWorkoutButton(_ sender: UIButton) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = #colorLiteral(red: 0.003112022998, green: 0.5457381606, blue: 0.5681524873, alpha: 1)
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
